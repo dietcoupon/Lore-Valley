7e84a0d35d86fc0abb8b0fac6def7c9e6cb2a6da
Tracy <tracy.cogsdill@gmail.com>
2011-08-17 18:11:34 -0400

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoreValley::Application.config.secret_token = '8023b84e76dd4ac2914633347038f2ce5949f9a0cc242b6a7b9cddf3342c354ace82024818cd3a29d5861b7a47b72bd1d53d60641850b3e46258a606c85186b0'
